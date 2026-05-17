5a75239dec3803dd0c26ca4796c80ee96e847a95
Devandra Albansyah Azhar <devandraalbansyahazhar@gmail.com>
2026-05-17 23:32:52 +0700

    Update AuthController: Redirect otomatis sesuai role

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -22,8 +22,18 @@ class AuthController extends Controller
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
             
-            // Sementara kita arahkan ke halaman home bawaan Laravel dulu
-            return redirect()->intended('/'); 
+            // Logika Redirect Otomatis Berdasarkan Role
+            $role = Auth::user()->role;
+
+            if ($role == 'Ketua KK') {
+                return redirect('/ketuakk/dashboard');
+            } elseif ($role == 'Ketua Lab') {
+                return redirect('/ketualab/dashboard');
+            } elseif ($role == 'Anggota') {
+                return redirect('/anggota/dashboard');
+            } else {
+                return redirect('/'); 
+            }
         }
 
         return back()->with('loginError', 'Username atau Password salah!');
